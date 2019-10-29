1df19e6e048be86e904d5d0d71353987992d512b
Basit Parkar <parkarbasit3009@gmail.com>
2019-10-29 19:40:12 +0400

    Initial Commit

---
+++ TestMPIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestMPIOS
+//
+//  Created by Basit Parkar on 10/29/19.
+//  Copyright © 2019 Basit Parkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
