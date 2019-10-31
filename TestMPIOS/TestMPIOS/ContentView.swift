38de83d28afa7fa7f742fdf14250b8f2f8eae04c
Mina Eweida <mina.eweida@careem.com>
2019-10-31 16:41:14 +0400

    Setting up iOS and renaming sharedLib to core

--- TestMPIOS/TestMPIOS/ContentView.swift
+++ TestMPIOS/TestMPIOS/ContentView.swift
@@ -7,10 +7,11 @@
 //
 
 import SwiftUI
+import Core
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        Text(CommonKt.createAppScreenMessage())
     }
 }
 
