f0d86f19a37a7380740a1788dc0adb945f4d3474
Mina Eweida <mina.eweida@careem.com>
2019-10-31 16:41:14 +0400

    Setting up iOS and renaming sharedLib to core

---
+++ TestMPIOS/TestMPIOS/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Core
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = CommonKt.createAppScreenMessage()
+        view.addSubview(label)
+    }
+}
